84465a9258824748200b05d8c199aa63b386f7ef
AleXutzZu <davidalex_robert@yahoo.com>
2022-07-09 21:42:48 +0300

    Fixed names

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import React, {FC} from "react";
 const Header: FC = () => {
     return (
         <header className={"flex items-center justify-center bg-blue-50 rounded-b-2xl h-[10%]"}>
-            <h1 className={"text-blue-600 text-6xl font-bold"}>My Notes</h1>
+            <h1 className={"text-blue-600 text-6xl font-bold"}>Notewind</h1>
         </header>
     );
 }
